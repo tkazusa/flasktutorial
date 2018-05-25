252155d707b9fc9aa614dc3fc66c6537e199913f
tkazusa <takekazusa@gmail.com>
2018-05-25 13:27:47 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import request, Flask
+app = Flask(__name__)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+
+def do_the_login():
+    return "do"
+
+def show_the_login_form():
+    return "show"
