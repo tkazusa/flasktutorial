b9a3944e3abeaad986cdea4841aa80d033356433
tkazusa <takekazusa@gmail.com>
2018-05-26 18:00:53 +0000

    hello world

--- hello.py
+++ hello.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!' 
+    return '''
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8"/>
+            <link rel="stylesheet" href="/static/style.css"/>
+          </head>
+          <body>
+            <h1>Hello World</h1>
+          </body>
+        </html>
+        '''
 
 
 @app.route('/hoge', methods=['POST'])
