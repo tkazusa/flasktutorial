6f80cea157d98d55557ee2fe35fac2cc7aad16f1
tkazusa <takekazusa@gmail.com>
2018-05-27 14:38:57 +0000

    mod

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, rrender_template
 app = Flask(__name__)
 
 
 @app.route('/title/<title>')
 def title(title):
     return render_template('index.html', title=title)
+
 
 @app.route('/hoge', methods=['POST'])
 def Hoge():
@@ -13,6 +14,11 @@ def Hoge():
 
 @app.route('/hoge', methods=['GET'])
 def hoge():
+    return 'hoge'
+
+@app.route('/search')
+def search():
+    q = request.args.get('q', '') 
     return 'hoge'
 
 
