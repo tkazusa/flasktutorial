9d8c3948fa2ed95fdebde0be5bcf0dcb617316d9
tkazusa <takekazusa@gmail.com>
2018-05-28 16:07:05 +0000

    mod

--- hello.py
+++ hello.py
@@ -3,6 +3,12 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Hello World')
+
+
 @app.route('/login', methods=['GET'])
 def render_form():
     return render_template('login.html')
